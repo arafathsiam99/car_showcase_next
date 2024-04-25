ee564174b9374d5b826f8bfafd9b4d81537b40cd
arafathsiam99 <siammdarafathislam@gmail.com>
2024-04-25 20:09:14 +0600

    searchbox updated

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,12 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters:FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
     const headers = {
         'X-RapidAPI-Key': '5acd27385emsh29652f9cccbcd1fp186c8fjsnfd6ac91aa89c',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
         }
-    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',{
+    const response = await fetch (`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,{
         headers:headers,
     });
     const result = await response.json();
